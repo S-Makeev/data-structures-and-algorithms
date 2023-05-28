95fea57bbd2adeecb23059a3ecc0b345eb3f277e
Slava Makeev <unclearnuclear@yahoo.com>
2023-05-28 01:48:41 +0000

    added extra

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -116,7 +116,10 @@ console.log(a) prints [1, 2, 3, 4]
 
 const append = (arr1, arr2) => {
   // Solution code here...
-
+  for(let i = 0; i < arr2.length; i++)
+  {
+    arr1.push(arr2[i]);
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -176,7 +179,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
