5ce961219baf6c65b549ffc8c69099ff6a44249a
Slava Makeev <unclearnuclear@yahoo.com>
2023-06-28 22:08:35 +0000

    stack_queue_brackets

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,25 @@
 from data_structures.queue import Queue
+from data_structures.stack import Stack
+
+def multi_bracket_validation(_str):
+    coolStack = Stack()
+
+    left_bracket = ['(', '{', '[']
+    right_bracket = [')', '}', ']']
 
 
-def multi_bracket_validation():
-    pass
+    for char in _str:
+        if char in left_bracket:
+            coolStack.push(char)
+        elif char in right_bracket:
+            if coolStack.is_empty():
+                return False
+            top = coolStack.pop()
+            if left_bracket.index(top) != right_bracket.index(char):
+                return False
+
+    return coolStack.is_empty()
+
+
+
+
