06d85827e031835ec367fd4c5f9668daf0d8e878
Slava Makeev <unclearnuclear@yahoo.com>
2023-06-30 22:58:08 +0000

    trees are growing

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,15 +1,41 @@
 from data_structures.binary_tree import BinaryTree
-
+from data_structures.binary_tree import Node
 
 class BinarySearchTree(BinaryTree):
-    """
-    Put docstring here
-    """
+    def __init__(self, root=None):
+        self.root = root
 
-    def __init__(self):
-        # initialization here
-        pass
 
-    def some_method(self):
-        # method body here
-        pass
+    def add(self, value):
+        if not self.root:
+            self.root = Node(value)
+            return
+
+        current = self.root
+        while True:
+            if value < current.value:
+                if current.left:
+                    current = current.left
+                else:
+                    current.left = Node(value)
+                    break
+            elif value > current.value:
+                if current.right:
+                    current = current.right
+                else:
+                    current.right = Node(value)
+                    break
+            else:
+                break
+
+        # There's a root => check value, create a new_node
+    def contains(self, value):
+       current = self.root
+       while current:
+           if current.value == value:
+               return True
+           elif value < current.value:
+               current = current.left
+           else:
+               current = current.right
+       return False
