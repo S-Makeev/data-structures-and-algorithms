99301d7b7fb168f3ca2030f063f892c2601cacbd
Slava Makeev <unclearnuclear@yahoo.com>
2023-07-24 21:17:16 +0000

    hashmap-repeated-word

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,19 @@
 from data_structures.hashtable import Hashtable
+import re
+#TODO Write a function called repeated word that finds the first word to occur more than once in a string
+# Arguments: string
+# Return: string
 
+def first_repeated_word(_str):
 
-def first_repeated_word():
-    pass
+    stripped_str = re.sub(r'[^\w\s]', '', _str)
+    word_str = Hashtable()
+
+    words = re.findall(r'\b\w+\b', stripped_str.lower())
+
+    for word in words:
+        if word_str.has(word):
+            return word
+        else:
+            word_str.set(word, True)
+    return None
