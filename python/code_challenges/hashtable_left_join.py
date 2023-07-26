f07bf710bf79e1748a0c7016837799e2c12ace4d
Slava Makeev <unclearnuclear@yahoo.com>
2023-07-26 21:44:24 +0000

    hash-left-join

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,14 @@
 from data_structures.hashtable import Hashtable
 
 
-def left_join():
-    pass
+def left_join(synonyms, antonyms):
+    result = []
+    for key in synonyms.keys():
+        synonym_value = synonyms[key]
+        antonym_value = antonyms.get(key, "NONE")
+        result.append([key, synonym_value, antonym_value])
+    return result
+
+
+
+
